ca4bcf047f2af16c1cfcc2d3347c31ebe6773ad2
philippe CHIRIE <p.chirie@laposte.net>
2019-11-25 17:45:55 +0100

    LastUpdate

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,69 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.geocoded
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
+      }
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    respond_to do |format|
+      if @flat.save
+        format.html { redirect_to @flat, notice: 'Flat was successfully created.' }
+        format.json { render :show, status: :created, location: @flat }
+      else
+        format.html { render :new }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @flat.update(flat_params)
+        format.html { redirect_to @flat, notice: 'Flat was successfully updated.' }
+        format.json { render :show, status: :ok, location: @flat }
+      else
+        format.html { render :edit }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    respond_to do |format|
+      format.html { redirect_to flats_url, notice: 'Flat was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address)
+  end
+end
